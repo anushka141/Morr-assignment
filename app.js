1af49f07d2e1019f1c8c9c40127408652fab2879
ANUSHKA AGRAWAL <61308296+anushka141@users.noreply.github.com>
2022-01-12 23:25:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const num = document.getElementById("number");
+const date = document.getElementById("date");
+const code = document.getElementById("code");
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const btn = document.getElementById("btn");
+
+btn.addEventListener(onclick,()=>{
+
+});
+
+function checknum() {
+  num.style.outlineColor = "green";
+}
+function checkdate() {
+  date.style.outlineColor = "green";
+}
+function checkcode() {
+  code.style.outlineColor = "green";
+}
+function checkfirst() {
+  first.style.outlineColor = "green";
+}
+function checklast() {
+  last.style.outlineColor = "green";
+}
+
